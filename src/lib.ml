5d1b9ef8f55841427d77c9ad4138d105d69c4364
Darren Ldl <darrenldldev@gmail.com>
2019-12-04 14:51:39 +1100

    More sequences

--- src/lib.ml
+++ src/lib.ml
@@ -1,6 +1,8 @@
 module type B = sig
   type t
 
+  val of_int : int -> t
+
   val add : t -> t -> t
 
   val sub : t -> t -> t
@@ -11,7 +13,39 @@ module type B = sig
 end
 
 module type S = sig
+  type t
+
+  val nat : t Seq.t
+
+  val range_exc : ?incre:t -> t -> t -> t Seq.t
 end
 
-module Make (B : B) = struct
+module Make (B : B) : S = struct
+  type t = B.t
+
+  let zero = B.of_int 0
+
+  let one = B.of_int 1
+
+  let pred x = B.(sub x one)
+
+  let succ x = B.(add x one)
+
+  let nat =
+    OSeq.iterate zero succ
+
+  let rec range_exc ?(incre = one) start end_exc =
+    if start < end_exc then
+      OSeq.cons start (range_exc (B.add start incre) end_exc)
+    else
+      OSeq.empty
+
+  let range_inc start end_inc = range_exc start (succ end_inc)
+
+  let zero_to_n_exc n = range_exc zero n
+
+  let zero_to_n_inc n = range_inc zero n
+
+  let modulo n =
+    OSeq.cycle (zero_to_n_exc n)
 end
